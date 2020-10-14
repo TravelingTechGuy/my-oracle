f3a997743c443d6c3733fe50da6e2397b8528aaa
Guy Vider <guy@guyvider.com>
2020-10-14 16:13:01 -0700

    renamed test files

--- test/getBTCCap.js
+++
@@ -1,9 +0,0 @@
-const {getBTCCap} = require('../src/getBTCCap');
-
-describe('getting btc cap', () =>{
-  it('should get bitcoin market cap as integer', async () => {
-    const result = await getBTCCap();
-    assert(Number.isInteger(result));
-    assert(result > 0);
-  });
-});
